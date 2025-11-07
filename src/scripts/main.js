4a9a4cc8abd3c63990d76f49baf004eb30c723c9
VM93 <vovamuzyka93@gmail.com>
2025-11-08 01:04:44 +0200

    have done a validation step

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,14 +2,21 @@
 
 // write your code here
 
-const convertation = () => {
-  const elements = [...document.querySelectorAll(`span.population`)];
+// const convertation = () => {
+//   const elements = [...document.querySelectorAll(`span.population`)];
 
-  return elements.map((span) => Number(span.textContent.replace(/,/g, '')));
-};
+//   return elements.map((span) => Number(span.textContent.replace(/,/g, '')));
+// };
 
-const total = convertation().reduce((a, b) => a + b);
-const average = convertation().reduce((a, b) => a + b) / convertation().length;
+const elements = [...document.querySelectorAll(`span.population`)];
+const convertation = elements.map((x) => {
+  if (!isNaN(x.textContent.replace(/,/g, ''))) {
+    return +x.textContent.replace(/,/g, '');
+  }
+});
+
+const total = convertation.reduce((a, b) => a + b);
+const average = convertation.reduce((a, b) => a + b) / convertation.length;
 
 document.querySelector(`span.average-population`).textContent =
   Math.floor(average).toLocaleString('en-US');
