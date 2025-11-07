4051cb1565e7438b839d993b7b12516e9ac8cb01
VM93 <vovamuzyka93@gmail.com>
2025-11-07 23:47:43 +0200

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
 // write your code here
+
+const convertation = () => {
+  const elements = [...document.querySelectorAll(`span.population`)];
+
+  return elements.map((span) => Number(span.textContent.replace(/,/g, '')));
+};
+
+const total = convertation().reduce((a, b) => a + b);
+const average = convertation().reduce((a, b) => a + b) / convertation().length;
+
+document.querySelector(`span.average-population`).textContent =
+  Math.floor(average).toLocaleString('en-US');
+
+document.querySelector(`span.total-population`).textContent =
+  total.toLocaleString('en-US');
